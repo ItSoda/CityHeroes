63ef047324510626c1b5c50cc0ffce33a51ebab8
It Soda <nikshchegilskiy@gmail.com>
2023-11-11 22:52:07 +0300

    fix| media

--- CityHeroes/urls.py
+++ CityHeroes/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -24,3 +26,4 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_url
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
