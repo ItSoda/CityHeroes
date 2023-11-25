aca7438887ff241707a5237e64584e13a769784f
It Soda <nikshchegilskiy@gmail.com>
2023-11-25 21:40:42 +0300

    update| cors add origins

--- CityHeroes/settings.py
+++ CityHeroes/settings.py
@@ -295,7 +295,8 @@ YOOKASSA_SECRET_KEY = env("YOOKASSA_SECRET_KEY")
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
 ]
 
 CORS_ALLOW_METHODS = [
