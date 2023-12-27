adccce3be1a5e8b1c95fb331d504bc1e8f84b574
It Soda <nikshchegilskiy@gmail.com>
2023-12-27 13:50:53 +0300

    update| rest

--- chat/views.py
+++ chat/views.py
@@ -10,34 +10,43 @@ from .serializers import RoomSerializer, RoomCreateSerializer
 class CreateRoom(APIView):
     serializer_class = RoomCreateSerializer
     def post(self, request, uuid):
-        username = self.request.POST.get("username", "")
-        url = self.request.POST.get("url", "")
+        try:
+            username = self.request.POST.get("username", "")
+            url = self.request.POST.get("url", "")
 
-        Room.objects.create(uuid=uuid, client=username, url=url)
-        
-        return Response({"message": "room created"}, status=status.HTTP_201_CREATED)
+            Room.objects.create(uuid=uuid, client=username, url=url)
+            
+            return Response({"data": "room created"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"data": f"error - {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChatAdmin(APIView):
     serializer_class = RoomSerializer
     def get(self, request, *args, **kwargs):
-        rooms = Room.objects.all()
-        users = Users.objects.filter(is_stuff=True)
+        try:
+            rooms = Room.objects.all()
+            users = Users.objects.filter(is_stuff=True)
 
-        return Response({"rooms": rooms, "users": users}, status=status.HTTP_200_OK)
+            return Response({"rooms": rooms, "users": users}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"data": f"error - {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
     
 
 class Room(APIView):
     serializer_class = RoomSerializer
     def get(self, request, uuid,  *args, **kwargs):
-        room = Room.objects.get(uuid=uuid)
+        try:
+            room = Room.objects.get(uuid=uuid)
 
-        if room.status == Room.WAITING:
-            room.status = Room.ACTIVE
-            room.agent = self.request.user
-            room.save()
+            if room.status == Room.WAITING:
+                room.status = Room.ACTIVE
+                room.agent = self.request.user
+                room.save()
 
-        return Response({"room": room}, status=status.HTTP_200_OK)
+            return Response({"room": room}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"data": f"error - {str(e)}"}, status=status.HTTP_404_NOT_FOUND)
         
 
 
